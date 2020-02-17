290d0c339807ff53257e8b7b6720e325f7ffc172
Mark Tsai <mx0910@gmail.com>
2020-02-17 17:13:08 +0800

    fix(2018): add gh-pages and debug

--- 2018/src/index.js
+++ 2018/src/index.js
@@ -1,10 +1,7 @@
 import './styles/index.scss';
 import template from './views/template.pug';
 
-const context = { message: 'Hello World' };
-
 if (document && document.body) {
-    const main = document.getElementById('main');
-    console.log(main);
-    main.innerHTML = template(context);
+  const main = document.getElementById('main');    
+  main.innerHTML = template();
 }
