68a26868a553b25376a75533d8abbd2dd491bbe2
Mark Tsai <mx0910@gmail.com>
2023-05-14 22:22:21 +0800

    feat(2023): modify layout

--- 2023/src/main.ts
+++ 2023/src/main.ts
@@ -1,9 +1,13 @@
-import './style.css';
+import './style.scss';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="flower">
-    <div class="sub-flower">
-    </div>
+  <div class="container">
+    <div class="flower1"></div>
+    <div class="flower2"></div>
+    <div class="flower3"></div>
+    <div class="flower4"></div>
+    <div class="flower5"></div>
   </div>
+  <h1>Happy Mother's Day!</h1>
 `;
 
